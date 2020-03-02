ec8b988c6d0169ba49367297c3e292d421f632b8
Sundeep Charan Ramkumar <sunguru98@yahoo.co.in>
2020-03-02 10:01:00 +0530

    Feature users added user routes

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res) {
   res.send("Hello world");
 });
 
+app.get("/users", function(req, res) {
+  res.send("Users route");
+});
+
 app.listen(1234, function() {
   console.log("Server connected");
 });
