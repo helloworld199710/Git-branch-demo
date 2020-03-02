4669221e5b055808fe47fbf2ea12cfc8af969bd3
Sundeep Charan Ramkumar <sunguru98@yahoo.co.in>
2020-03-02 10:08:06 +0530

    User branch added middleware

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require("express");
-
+var authenticate = require("./middleware/authenticate");
 var app = express();
 
 app.get("/", function(req, res) {
   res.send("Hello world");
 });
 
-app.get("/users", function(req, res) {
+app.get("/users", authenticate, function(req, res) {
   res.send("Users route");
 });
 
